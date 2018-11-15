b83747f8665188fab4bc2897d545aeaf40ade032
D.C <tdchap@gmail.com>
2018-11-15 15:06:45 -0600

    Initial commit

--- src/org/launchcode/java/demos/java4python/Hello.java
+++ src/org/launchcode/java/demos/java4python/Hello.java
@@ -4,7 +4,8 @@ package org.launchcode.java.demos.java4python;
  * From "Java for Python Programmers"
  */
 public class Hello {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello, World");
     }
 }
